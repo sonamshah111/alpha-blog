ed23213caf3911346fc2d4e7f18c677b60153617
Sonam Shah <sonam.shah1687@gmail.com>
2020-10-07 15:22:49 -0500

    New article form and save method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,18 @@ class ArticlesController < ApplicationController
 	def index
 	  @articles = Article.all		
 	end
+	
 	def show
 		@article = Article.find(params[:id])
 	end
+  
+  def new
+  
+  end
+  
+  def create
+  	@article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save
+  	redirect_to @article
+  end 	
 end
